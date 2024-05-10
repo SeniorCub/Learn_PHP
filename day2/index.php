fbffedfec9689923d31e9ab79a7647f146d91177
SeniorCub <farinderif@gmail.com>
2024-05-10 18:11:24 +0100

    "Added examples of various PHP array functions: array\_merge(), array\_pop(), array\_push(), array\_replace(), array\_search(), array\_slice(), array\_combine(), array\_diff()"

--- day2/index.php
+++ day2/index.php
@@ -81,6 +81,55 @@
                     array("Saab", 5, 2),
                     array("Land Rover", 17, 15)
                );
-               print_r($newcarss[0][1][0])
+               print_r($newcarss[0][1][0]);
+               echo ('<br>');
+               echo ('<br>');
+               echo ('<br>');
+
      // Check out more functions to perform with array
+          // PHP array_merge() Function     
+          $a1=array("red","green");
+          $a2=array("blue","yellow");
+          print_r(array_merge($a1,$a2));
+          echo ('<br>');
+
+          // PHP array_pop() Function
+          $a=array("red","green","blue");
+          array_pop($a);
+          print_r($a);
+          echo ('<br>');
+
+          // PHP array_push() Function
+          $a=array("red","green");
+          array_push($a,"blue","yellow");
+          print_r($a);
+          echo ('<br>');
+
+          // PHP array_replace() Function
+          $a1=array("red","green");
+          $a2=array("blue","yellow");
+          print_r(array_replace($a1,$a2));
+          echo ('<br>');
+
+          // PHP array_search() Function
+          $a=array("a"=>"red","b"=>"green","c"=>"blue");
+          echo array_search("red",$a);
+
+          // PHP array_slice() Function
+          $a=array("red","green","blue","yellow","brown");
+          print_r(array_slice($a,2));
+          echo ('<br>');
+
+          // PHP array_combine() Function
+          $fname=array("Peter","Ben","Joe");
+          $age=array("35","37","43");
+          $c=array_combine($fname,$age);
+          print_r($c);
+
+          // PHP array_diff() Function
+          $a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+          $a2=array("e"=>"red","f"=>"green","g"=>"blue");
+          $result=array_diff($a1,$a2);
+          print_r($result);
+          
 ?>
