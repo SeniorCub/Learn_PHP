aad6fba440cd255ba18364a23f6d1fe6abdb3029
seniorcub <farinderif@gmail.com>
2024-07-04 11:08:12 +0100

    Done with todays class

---
+++ day6/index.php
@@ -0,0 +1,23 @@
+<?php
+     $break = "<br>";
+     // Control Structure are used to control the flow of a program based on specific conditions or logic
+
+     // If-Else Statements: Used to execute different blocks of code based on a condition. 
+     if (true) {
+          echo(true);
+     };
+     echo($break);
+     $var = null;
+     if ($var) {
+          echo("true");
+     } else {
+          echo("false");
+     }
+     echo($break);
+     $var1 = 5;
+     if ($var1) {
+          echo("true");
+     } else {
+          echo("false");
+     }
+?>
