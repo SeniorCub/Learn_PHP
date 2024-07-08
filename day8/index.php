b0c850c35be204bb719d554c893c7bb2862c3cc6
seniorcub <farinderif@gmail.com>
2024-07-08 11:02:41 +0100

    done with todays class

---
+++ day8/index.php
@@ -0,0 +1,97 @@
+<?php
+     $break = "<br>";
+     // Function
+     // Function is a block of code that only runs when it is called
+     // You can pass data, known as parameters, into a function
+
+     echo("<h1>Functions</h1>");
+
+     // Function without parameters
+     echo("<h4>Functions without parameters</h4>");
+     function sayHello(){
+          echo("Hello User");
+          echo $break;
+     };
+     sayHello();
+     echo $break;
+     echo $break;
+
+
+     // Function with parameters
+     echo("<h4>Functions with parameters</h4>");
+     function greetUser($name) {
+          echo("Hello " . $name);
+          echo $break;
+     };
+     greetUser("SeniorCub");
+     echo $break;
+     greetUser("Roheemah");
+     echo $break;
+     echo $break;
+
+
+     // Function to add two numbers
+     echo("<h4>Function to add two numbers</h4>");
+     function addNum($a, $b){
+          return($a + $b);
+     }
+     echo(addNum(3,5));
+     echo $break;
+     echo $break;
+
+     // Function with default parameter
+     echo("<h4>Function with default parameter</h4>");
+     $x = 5;
+     $y = 13;
+     function add($x, $y){
+          return($x + $y);
+     }
+     echo(add($x, $y));
+     echo $break;
+     function adds($v = 3, $g = 2){
+          return($v + $g);
+     }
+     echo(adds());
+     echo $break;
+     echo(adds(4, 6));
+     echo $break;
+     echo $break;
+
+     // Function to multiple two numbers
+     echo("<h4>Function to multiple two numbers</h4>");
+     function multiply($a = 3, $b = 5){
+          return($a * $b);
+     }
+     echo(multiply());
+     echo $break;
+
+     // Create a program to determine which number is small within two numbers
+     echo("<h4>Function to determine which number is small within two numbers</h4>");
+     function smallNum($a, $b){
+          if ($a < $b){
+               return($a . " is less than " . $b);
+          } else {
+               return($b . " is less than " . $a);
+          }
+     }
+     echo(smallNum(3, 5));
+     echo $break;
+     echo(smallNum(25, 13));
+     echo $break;
+     echo $break;
+
+     // Function with variable length arguments
+     echo("<h4>Function with variable length arguments</h4>");
+     // "..." means splat. used to declare continous variable
+     function sumAll(...$numbers){
+          $sum = 0;
+          foreach ($numbers as $number) {
+               $sum += $number;
+          }
+          return($sum);
+     };
+     echo(sumAll(2 , 3, 1, 6, 9));
+
+     // Check if a number is even or odd
+     // calculate the sum of numbers from 1 to 10
+?>
