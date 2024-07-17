487ecfbf88c135de118bff75f5ebf668976bf8d5
SeniorCub <farinderif@gmail.com>
2024-07-17 14:39:58 +0100

    done with todays class. now using windows for PHP

--- day15/connect.php
+++ day15/connect.php
@@ -1,3 +1,10 @@
 <?php
-     $conn = mysqli_connect("localhost","root","","FirstWork");
+     $servername = "localhost";
+     $username = "root";
+     $password = ""; // Leave this empty if you have not set a password
+     $dbname = "FirstWork";
+     $port = 3307; // Add the new port number
+     
+     // Create connection
+     $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 ?>
--- day15/index.php
+++ day15/index.php
@@ -1,7 +1,7 @@
 <?php
      include "connect.php";
 
-     
+     $selectAll = mysqli_query($conn, "SELECT * FROM day_14");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,6 +11,49 @@
      <title>Document</title>
 </head>
 <body>
-     
+     <table border="1">
+          <thead>
+               <tr>
+                    <th>S/N</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>email</th>
+                    <th>Dp</th>
+               </tr>
+          </thead>
+          <?php
+               while($details = mysqli_fetch_assoc($selectAll)){
+          ?>
+          <tbody>
+               <tr>
+                    <td>
+                         <?php
+                              echo $details["id"];
+                         ?>
+                    </td>
+                    <td>
+                         <?php
+                              echo $details["fname"];
+                         ?>
+                    </td>
+                    <td>
+                         <?php
+                              echo $details["lname"];
+                         ?>
+                    </td>
+                    <td>
+                         <?php
+                              echo $details["email"];
+                         ?>
+                    </td>
+                    <td>
+                         <img src="Uploads/<?php echo $details["file"]?>" width="50px" alt="...">
+                    </td>
+               </tr>
+          </tbody>
+          <?php
+               }
+          ?>
+     </table>
 </body>
 </html>
