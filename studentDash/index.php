9a0f8c1913c92fbfcb19e0a22e4b6ea4cd133f05
SeniorCub <farinderif@gmail.com>
2024-07-29 22:32:53 +0100

    worked on delete fpr the admin

--- studentDash/index.php
+++ studentDash/index.php
@@ -2,6 +2,16 @@
      include "connect.php";
 
      $selectAll = mysqli_query($conn, "SELECT * FROM `studentdash`");
+
+     if (isset($_POST["delete"])){
+          $marticNo = $_POST["marticNo"];
+          echo($marticNo);
+          $delete = mysqli_query($conn, "DELETE FROM `studentdash` WHERE `marticNo` = '$marticNo'");
+          if ($delete){
+               echo('User Deleted Sucessfuly');
+               //header("location: profile.php");
+          }
+     }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +24,12 @@
                img{
                     width: 50px;
                     height: 50px;
+               }
+               input{
+                    border: none;
+                    color: #000;
+                    outline: none;
+                    background-color: transparent;
                }
           }
      </style>
@@ -42,19 +58,22 @@
                while($details = mysqli_fetch_assoc($selectAll)){
           ?>
           <tr>
-               <td class="dp">
-                    <img src="Uploads/<?php echo $details["image"] ?>" alt="profile picture">
-               </td>
-               <td class="name"><?php echo($details['fname'] . " " .  $details['lname']); ?></td>
-               <td class="email"><?php echo $details['email']; ?></td>
-               <td class="matricNo"><?php echo $details['marticNo']; ?></td>
-               <td class="age"><?php echo $details['age']; ?></td>
-               <td class="gender"><?php echo $details['gender']; ?></td>
-               <td class="level"><?php echo $details['level']; ?></td>
-               <td class="department"><?php echo $details['department']; ?></td>
-               <td class="city"><?php echo $details['city']; ?></td>
-               <td class="state"><?php echo $details['state']; ?></td>
-               <td class="country"><?php echo $details['country']; ?></td>
+               <form action="" method="POST">
+                    <td class="dp">
+                         <img src="Uploads/<?php echo $details["image"] ?>" alt="profile picture">
+                    </td>
+                    <td class="name"><?php echo($details['fname'] . " " .  $details['lname']); ?></td>
+                    <td class="email"><?php echo $details['email']; ?></td>
+                    <td class="matricNo"><input type="text" name="marticNo" value="<?php echo $details['marticNo']; ?>" disabled></td>
+                    <td class="age"><?php echo $details['age']; ?></td>
+                    <td class="gender"><?php echo $details['gender']; ?></td>
+                    <td class="level"><?php echo $details['level']; ?></td>
+                    <td class="department"><?php echo $details['department']; ?></td>
+                    <td class="city"><?php echo $details['city']; ?></td>
+                    <td class="state"><?php echo $details['state']; ?></td>
+                    <td class="country"><?php echo $details['country']; ?></td>
+                    <td><button type="submit" class="btn" name="delete">Delete</button></td>
+               </form>
           </tr>
           <?php
                }
